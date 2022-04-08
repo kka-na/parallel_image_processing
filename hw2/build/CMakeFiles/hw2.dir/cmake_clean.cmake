fe2943ef083e7f99bf384f2eb3b64f3ddaa0e2c4
kka-na <kimgana96@gmail.com>
2022-04-08 12:48:29 +0900

    Git Ignore Update

---
+++ hw2/build/CMakeFiles/hw2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw2.dir/src/hw2.cpp.o"
+  "CMakeFiles/hw2.dir/src/hw2.cpp.o.d"
+  "hw2"
+  "hw2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
